12c18fee2afcf4ee36c50883769ce9995ca8851b
Graham Spencer <grahamspence@gmail.com>
2018-08-01 23:45:07 -0400

    exercise completed

--- crm.rb
+++ crm.rb
@@ -5,6 +5,18 @@ get "/" do
   erb :index
 end
 
+get "/contacts" do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get "/about" do
+  erb :about
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
 #
 # class CRM
 #

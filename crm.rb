eeb5f895ef3386a722e42150373385a3704336d1
Graham Spencer <grahamspence@gmail.com>
2018-08-04 18:08:04 -0400

    added edit contact part

--- crm.rb
+++ crm.rb
@@ -14,12 +14,21 @@ get "/contacts/new" do
   erb :new
 end
 
-get "/contacts/:id" do
+put "/contacts/:id" do
   @contact = Contact.find_by({id: params[:id].to_i})
   if @contact
     erb :show_contact
   else
     raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinantra::NotFound
   end
 end
 
